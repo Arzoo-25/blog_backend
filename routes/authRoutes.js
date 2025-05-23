a1c7ee2c8cbb80e543034730092c1c3f7f656a5f
Arzoo-25 <ajarzoo25@gmail.com>
2025-05-23 23:16:31 +0530

    Add logout route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,5 +4,8 @@ const {registerUser, loginUser} = require('../controllers/authController');
 
 router.post('/register',registerUser);
 router.post('/login', loginUser);
+router.post('/logout', (req, res) => {
+    res.status(200).json({ message: 'Logged out successfully. Please clear token on client side.' });
+  });
 
 module.exports = router;
